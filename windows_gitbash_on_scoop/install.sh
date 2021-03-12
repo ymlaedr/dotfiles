1d68ecdee9acabc6639075d47d636da7774d5daf
ymlaedr <ymlaedr@gmail.com>
2021-03-12 18:20:57 +0900

    update install.sh

--- windows_gitbash_on_scoop/install.sh
+++ windows_gitbash_on_scoop/install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#
+# windows_gitbash_on_scoop/install.sh
+# ============
+#
+# windowsへscoop経由でGitBashと各種設定ファイルをインストールする
+# 
+#
+# Usage:
+#
+#     install.sh [-h|--help|help|install]
 
 
 
@@ -13,11 +23,22 @@ BACKUP_DIR=${WORK_DIR}/backup/${TIMESTAMP}
 
 LOGFILE_PATH=${WORK_DIR}/backup/${TIMESTAMP}/execution.log
 
-
+SCOOP_EXTRAS_APPS=(
+    vscode
+    windows-terminal
+    xming
+)
 
 ## -----------------------------------------------------------------------------
 ## 共通処理
 ## -----------------------------------------------------------------------------
+
+function show_help() {
+    awk 'NR > 2 {
+        if (/^#/) { sub("^# ?", ""); print }
+        else { exit }
+    }' $0
+}
 
 function execution_log() {
     if [ "$1" = '' ]; then
@@ -26,6 +47,15 @@ function execution_log() {
         echo $1
         echo $1 >> ${LOGFILE_PATH}
     fi
+}
+
+function install_scoop_extras_apps() {
+    [ `scoop bucket list | grep extras` != '' ] && scoop bucket add extras
+    scoop update
+    for app_name in "${SCOOP_EXTRAS_APPS}"
+    do
+        scoop install ${app_name}
+    done
 }
 
 function replace() {
@@ -67,8 +97,17 @@ function replace() {
 ## メイン処理
 ## -----------------------------------------------------------------------------
 
-mkdir ${BACKUP_DIR}
-
-replace ${HOME}/.bashrc ${WORK_DIR}/bashrc
-replace ${HOME}/.ssh/config ${WORK_DIR}/ssh_config
-replace ${HOME}/config.xlaunch ${WORK_DIR}/config_xlaunch.xml
+case $1 in
+  "install")
+    mkdir ${BACKUP_DIR}
+    replace ${HOME}/.bashrc ${WORK_DIR}/envfiles/bashrc
+    replace ${HOME}/.ssh/config ${WORK_DIR}/envfiles/ssh_config
+    replace ${HOME}/config.xlaunch ${WORK_DIR}/envfiles/config_xlaunch.xml
+    ;;
+  "help" | "-h" | "--help") 
+    show_help;;
+  "")
+    echo "実行したい処理を第一引数で渡してください。";;
+  *) 
+    echo "該当する処理がありません。"
+esac
